6559a0f1cbf229fcece3db4f073947c69013ee31
DmitryMilton <dl.qbit@gmail.com>
2020-04-11 12:08:11 +0700

    lesson_13

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,50 +11,30 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.SetBufferSize(120, 30);
+			VerticalLine vl = new VerticalLine(0, 10, 5, '%');
+			Draw(vl);
 
-            // Отрисовка рамки
-            HorizontalLine topLine = new HorizontalLine(0, 118, 0, '+');
-            HorizontalLine bottomLine = new HorizontalLine(0, 118, 28, '+');
-            VerticalLine leftLine = new VerticalLine(0, 28, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 28, 118, '+');
-            topLine.Draw();
-            bottomLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
+			Point p = new Point(4, 5, '*');
+			Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+			Draw(fSnake);
+			Snake snake = (Snake)fSnake;
 
-            // Отрисовка точек
-            Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();
+			HorizontalLine hl = new HorizontalLine(0, 5, 6, '&');
 
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-            Point food = foodCreator.CreateFood();
-            food.Draw();
+			List<Figure> figures = new List<Figure>();
+			figures.Add(fSnake);
+			figures.Add(vl);
+			figures.Add(hl);
 
-            Console.CursorVisible = false;
-            while (true)
-            {
-                if (snake.Eat(food))
-                {
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-                }
-                else
-                {
-                    //snake.Move();
-                }
+			foreach (var f in figures)
+			{
+				f.Draw();
+			}
+		}
 
-                snake.Move();
-                Thread.Sleep(100);
-
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey(true);
-                    snake.HandleKey(key.Key);
-                }
-            }
-            Console.ReadLine();
-        }      
-    }
+		static void Draw(Figure figure)
+		{
+			figure.Draw();
+		}
+	}
 }
